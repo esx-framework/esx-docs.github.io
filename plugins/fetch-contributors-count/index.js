b35f8474fbdb8e64356196b8c01d9b765ea4d09d
Mycroft <kaseyfitton@hotmail.co.uk>
2022-01-28 18:15:03 +0000

    change git branch

--- plugins/fetch-contributors-count/index.js
+++ plugins/fetch-contributors-count/index.js
@@ -4,7 +4,7 @@ module.exports = () => ({
   name: "fetch-contributors-count",
   async loadContent() {
     const response = await nodeFetch(
-      `https://raw.githubusercontent.com/esx-framework/esx-legacy/master/.all-contributorsrc`,
+      `https://raw.githubusercontent.com/esx-framework/esx-legacy/main/.all-contributorsrc`,
     )
 
     const data = await response.json()
